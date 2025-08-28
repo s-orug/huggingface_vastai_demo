22951a055da50c3e182870cc9ec0e891655a8bd5
s-orug <sdroruganti@gmail.com>
2025-08-28 13:57:20 -0500

    Removing envs

--- setup.sh
+++ setup.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-set -e
-
-# Create virtual environment if it doesn't exist
-if [ ! -d "venv" ]; then
-    echo "[*] Creating virtual environment..."
-    python3 -m venv venv
-fi
-
-# Activate venv
-echo "[*] Activating virtual environment..."
-source venv/bin/activate
 
 # Upgrade pip
 echo "[*] Upgrading pip..."
